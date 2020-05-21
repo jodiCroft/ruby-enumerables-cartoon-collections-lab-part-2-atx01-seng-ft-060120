18f92315a7e17a64fa116628a9f0d3c143ca1660
Jodi Croft <jodipruett@gmail.com>
2020-05-21 16:04:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? do |string.length|
+  planeteer_calls.any? do |string|
     string.length > 4
   end
 end
