44e5039462dfd5c95abbd1acfb5099ff8ae1aa37
Jodi Croft <jodipruett@gmail.com>
2020-05-21 16:18:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |string|
-    string == valid_calls
+    string.include?(valid_calls)
   end
 end
